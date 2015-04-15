b2791e050cf127adf3d2c1af703e18c42c5498c5
Kauser Sarker <KauserSarker@Kausers-MacBook-Pro.local>
2015-04-15 08:56:10 +0600

    CC2

---
+++ block/gui/form4.php
@@ -0,0 +1,12 @@
+<form role="form" class="form-inline">
+      <label for="exampleInputEmail12">Email address</label>
+      <input type="email" id="exampleInputEmail12" placeholder="Enter email">
+
+      <label for="exampleInputPassword12">Password</label>
+      <input type="password" id="exampleInputPassword12" placeholder="Password">
+
+      <label class="checkbox"><input type="checkbox"> Check me out</label>
+
+      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="reset" class="btn btn-default">Reset</button>
+</form>
