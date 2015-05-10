5ddaba2f69ac2408f289cfe21a9a29d22ac3f25c
parvez210 <parvez08eee@gmail.com>
2015-05-10 12:11:57 +0600

    added

--- js/scripts.js
+++ js/scripts.js
@@ -590,4 +590,62 @@ $('.uou-block-12c').each(function () {
 
 
 
+
+    $("#slider").owlCarousel({
+      singleItem: true
+    });
+
+    $("#clients-slider").owlCarousel({
+      items: 6
+    });
+
+    // BACKGROUND FOR EACH SLIDE
+    $( '#slider' ).each(function(){
+
+      var self = $(this),
+      slide = self.find( '.slide' );
+
+      // SET BG IMAGES
+      slide.each(function(){
+        var img =  $(this).find( '.background' );
+        if ( img.length > 0 ) {
+          $(this).css( 'background-image', 'url(' + img.attr( 'src' ) + ')' );
+          img.hide();
+        }
+      });
+    });
+
+    $(".swipebox").swipebox();
+  });
+
+  $(window).load(function(){
+    // ISOTOPE FILTERS
+    $('#portfolio').each(function() {
+      var $container = $('.portfolio-filters-content');
+      // initialize
+      $container.masonry({
+        itemSelector: 'article'
+      });
+
+      var filterFns = {};
+
+      $('#portfolio .filters li a').on( 'click', function() {
+        var filterValue = $( this ).attr('data-filter');
+        // use filterFn if matches value
+        filterValue = filterFns[ filterValue ] || filterValue;
+        $container.isotope({ filter: filterValue });
+
+        $('#portfolio .filters li a').each(function(){
+          $(this).removeClass("active");
+        });
+        $(this).addClass("active");
+
+        return false;
+      });
+    });
+  });
+
+
+
+
 }(jQuery));
