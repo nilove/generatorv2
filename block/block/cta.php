3a1c265fe8629aa03bf18967b8d97f94734359f0
Nilove <nilovecse05@gmail.com>
2015-05-14 15:15:24 +0600

    new template cpnverted to generator

---
+++ block/block/cta.php
@@ -0,0 +1,11 @@
+ <!-- Start CTA -->
+  <section id="cta">
+    <div class="container">
+      <div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0">
+        <h3 class="title pull-left">Love what you've seen? hire us!</h3>
+        <a href="#" class="btn btn-large btn-magenta pull-right">Send An Inquiry</a>
+      </div>
+    </div>
+    <div class="arrow"></div>
+  </section>
+  <!-- End CTA -->
