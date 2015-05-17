e9f0fa46bb849e12225a20544da52998c4c2d5ff
parvez210 <parvez08eee@gmail.com>
2015-05-18 00:26:26 +0600

    fixed restaurant page issure

--- js/scripts.js
+++ js/scripts.js
@@ -568,6 +568,41 @@ $('.uou-block-12c').each(function () {
 
 
 
+  $(".swipebox").swipebox();
+
+
+$(window).load(function(){
+  // ISOTOPE FILTERS
+  $('.uou-portfolio').each(function() {
+    var $container = $('.portfolio-filters-content');
+    // initialize
+    $container.masonry({
+      itemSelector: 'article'
+    });
+
+    var filterFns = {};
+
+    $('.uou-portfolio .filters li a').on( 'click', function() {
+      var filterValue = $( this ).attr('data-filter');
+      // use filterFn if matches value
+      filterValue = filterFns[ filterValue ] || filterValue;
+      $container.isotope({ filter: filterValue });
+
+      $('.uou-portfolio .filters li a').each(function(){
+        $(this).removeClass("active");
+      });
+      $(this).addClass("active");
+
+      return false;
+    });
+  });
+});
+
+
+
+
+
+
   // $("#slider").owlCarousel({
   //   singleItem: true
   // });
@@ -616,32 +651,7 @@ $('.uou-block-12c').each(function () {
     $(".swipebox").swipebox();
 
 
-  $(window).load(function(){
-    // ISOTOPE FILTERS
-    $('#portfolio').each(function() {
-      var $container = $('.portfolio-filters-content');
-      // initialize
-      $container.masonry({
-        itemSelector: 'article'
-      });
-
-      var filterFns = {};
-
-      $('#portfolio .filters li a').on( 'click', function() {
-        var filterValue = $( this ).attr('data-filter');
-        // use filterFn if matches value
-        filterValue = filterFns[ filterValue ] || filterValue;
-        $container.isotope({ filter: filterValue });
-
-        $('#portfolio .filters li a').each(function(){
-          $(this).removeClass("active");
-        });
-        $(this).addClass("active");
-
-        return false;
-      });
-    });
-  });
+  
 
 
 
